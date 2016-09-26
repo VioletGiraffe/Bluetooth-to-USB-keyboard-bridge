da722657f9b1af56e261374e80a9e3a6683cd321
VioletGiraffe <VioletGiraffe@users.noreply.github.com>
2016-09-26 12:01:42 +0300

    IAR project edited

--- EWARM/settings/usb-F3.usb-F3.cspy.bat
+++ EWARM/settings/usb-F3.usb-F3.cspy.bat
@@ -25,7 +25,7 @@ if not "%~1" == "" goto debugFile
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.5\common\bin\cspybat" -f "C:\Users\Alex\Downloads\usb-F3\usb-F3\EWARM\settings\usb-F3.usb-F3.general.xcl" --backend -f "C:\Users\Alex\Downloads\usb-F3\usb-F3\EWARM\settings\usb-F3.usb-F3.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.5\common\bin\cspybat" -f "C:\Development\Bluetooth-to-USB-keyboard-bridge\EWARM\settings\usb-F3.usb-F3.general.xcl" --backend -f "C:\Development\Bluetooth-to-USB-keyboard-bridge\EWARM\settings\usb-F3.usb-F3.driver.xcl" 
 
 @echo off 
 goto end 
@@ -34,7 +34,7 @@ goto end
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.5\common\bin\cspybat" -f "C:\Users\Alex\Downloads\usb-F3\usb-F3\EWARM\settings\usb-F3.usb-F3.general.xcl" "--debug_file=%~1" --backend -f "C:\Users\Alex\Downloads\usb-F3\usb-F3\EWARM\settings\usb-F3.usb-F3.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.5\common\bin\cspybat" -f "C:\Development\Bluetooth-to-USB-keyboard-bridge\EWARM\settings\usb-F3.usb-F3.general.xcl" "--debug_file=%~1" --backend -f "C:\Development\Bluetooth-to-USB-keyboard-bridge\EWARM\settings\usb-F3.usb-F3.driver.xcl" 
 
 @echo off 
 :end
